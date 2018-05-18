a5c677bc46234eeebabdaa2d3f735078b47f40bc
Hideichi Saito <binbin4649@gmail.com>
2018-05-18 19:29:07 +0900

    schema update.

---
+++ Config/Schema/point_books.php
@@ -0,0 +1,33 @@
+<?php 
+class PointBooksSchema extends CakeSchema {
+
+	public $file = 'point_books.php';
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $point_books = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'mypage_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'point_user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'point' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'credit' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'point_balance' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'credit_balance' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'pay_token' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 200, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'charge' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'reason' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 200, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'reason_id' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 200, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+}
---
+++ Config/Schema/point_users.php
@@ -0,0 +1,29 @@
+<?php 
+class PointUsersSchema extends CakeSchema {
+
+	public $file = 'point_users.php';
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $point_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'mypage_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'point' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
+		'credit' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
+		'available_point' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
+		'payjp_token' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'pay_interval' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+}
