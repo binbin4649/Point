261d65d0ad9fcce04b39af2c39e732a81b1477a0
Hideichi Saito <binbin4649@gmail.com>
2018-08-10 11:33:58 +0900

    updater fix.

--- Config/update/0.0.1/updater.php
+++ Config/update/0.0.1/updater.php
@@ -18,3 +18,9 @@ if($this->loadSchema('0.0.1', 'Point', 'point_books', $filterType = 'alter')) {
 } else {
 	$this->setUpdateLog('point_books テーブルの構造変更に失敗しました。', true);
 }
+
+if($this->loadSchema('0.0.1', 'Point', 'point_coupons', $filterType = 'alter')) {
+	$this->setUpdateLog('point_coupons テーブルの構造変更に成功しました。');
+} else {
+	$this->setUpdateLog('point_coupons テーブルの構造変更に失敗しました。', true);
+}
